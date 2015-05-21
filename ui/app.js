c7d2e4c38f0355ac52666b5ba2497c12f04c8766
John Tibble <john.tibble@ge.com>
2015-05-21 14:55:32 -0400

    Added UI-Router and UI-Bootstrap

--- ui/app.js
+++ ui/app.js
@@ -1,13 +1,16 @@
-var app = angular.module('app', ['chart.js']);
+var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'chart.js']);
 
-app.controller('HomeController', function($scope){
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise("/home");
+    
+    $stateProvider.state('home', {
+        url: "/home",
+        templateUrl: "app/home/home.html",
+        controller: 'HomeController'
+    }).state('stability', {
+        url: "/stability",
+        templateUrl: "app/stability/stability.html"
+    });
+    
 });
---
+++ ui/app/controllers.js
@@ -0,0 +1,11 @@
+app.controller('HomeController', function($scope){
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+});
