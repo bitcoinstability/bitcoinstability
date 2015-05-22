2dbee2ba482c0c6ed844f945c64c59d1b1cf2f75
John Tibble <john.tibble@ge.com>
2015-05-22 11:52:47 -0400

    Added datepickers to select a time-frame to look at only a subset of the Bitcoin historical price.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# bitcoinstability
-Home of the Bitcoin Stability Charts
+# Bitcoin Stability
+Home of the Bitcoin Stability Charts, inspired/supported by Reddit user "Azop" and http://www.reddit.com/r/BitcoinMarkets/comments/36rn9h/help_uazop_internet_his_excel/
 
 If you want to join, PM jtibble on reddit, twitter, or here on Github
 
 # Current status:
-Price data displayed from CoinDesk API
+Price data displayed from CoinDesk API, with date-selection-filtering
 
-![Screenshot](http://i.imgur.com/fIkCt4L.png "WIP Screenshot")
+![Screenshot](http://i.imgur.com/zKprLeb.png "WIP Screenshot")
 
--- ui/app/controllers.js
+++ ui/app/controllers.js
@@ -1,43 +1,81 @@
 app.controller('StabilityController', function($scope, $http){
     
-    var start = '2012-01-01';
-    var today = new Date();
-    var year = today.getFullYear();
-    var month = today.getMonth()+1;
+    $scope.startDate = new Date(2011, 0, 1);
+    $scope.endDate = new Date();
     
-    if( month < 10 ){
-        month = '0' + month;
+    $scope.datePickerOptions = {
+        initDate: new Date(2011, 0, 1)
+    };
+    
+    $scope.openStartDate = function($event){
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.startDateOpened = true;  
+    }
+    $scope.openEndDate = function($event){
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.endDateOpened = true;  
     }
     
-    var day = today.getDate();
+    $scope.fetchData = function(){
     
-    if( day < 10 ){
-        day = '0' + day;
-    }
-    
-    var end = year + '-' + month + '-' + day;
-    
-    var url = 'http://localhost/pricedata?start=' + start + '&end=' + end;
-    
-    $http.get(url).success( function(data){
-        var labels = [];
-        var values = [];
-        
-        var step = 10;
-        var index = 0;
-        for( var i in data.bpi ){
-            if( index % step == 0){
-                labels.push(i);
-                values.push(data.bpi[i]);
+        function getFormattedDate(date){
+            var year = date.getFullYear();
+            var month = date.getMonth()+1;
+            var day = date.getDate();
+            
+            if( month < 10 ){
+                month = '0' + month;
             }
-            index++;
+            
+            if( day < 10 ){
+                day = '0' + day;
+            }
+            
+            return year + '-' + month + '-' + day;
         }
         
-        $scope.data = [values];
-        $scope.labels = labels;
-        $scope.series = ['Price Data'];
-        $scope.onClick = function (points, evt) {
-            console.log(points, evt);
-        };
-    });
+        var start = getFormattedDate( $scope.startDate );
+        var end = getFormattedDate( $scope.endDate );
+
+        var url = 'http://localhost/pricedata?start=' + start + '&end=' + end;
+
+        $http.get(url).success( function(data){
+            var labels = [];
+            var values = [];
+
+            // Don't show all the data unless there are less than N points
+            var limit = 50;
+            var step = Math.ceil(Object.keys(data.bpi).length / limit);
+            
+            var index = 0;
+            for( var i in data.bpi ){
+                if( index % step == 0){
+                    labels.push(i);
+                    values.push(data.bpi[i]);
+                }
+                index++;
+            }
+
+            $scope.data = [values];
+            $scope.labels = labels;
+            $scope.series = ['Price Data'];
+            $scope.onClick = function (points, evt) {
+                console.log(points, evt);
+            };
+            $scope.options = {
+                animation: false,
+                showScale: true,
+                showTooltips: false,
+                pointDot: false,
+                datasetStrokeWidth: 0.5
+            };
+        });
+        
+    }
+    
+    $scope.fetchData();
 });
